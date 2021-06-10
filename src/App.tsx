d1125dd37df7886bbb21c479cfb020b2ed506142
Anjaliba Gohil <codexprt2@gmail.com>
2021-06-10 18:20:40 +0530

    drawer added

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,10 @@ const getProducts = async (): Promise<CartItemType[]> =>
 	await (await fetch("https://fakestoreapi.com/products")).json();
 
 const App = () => {
+	const [cartOpen, setCartOpen] = useState(false);
+	const [cartItems, setCartItems] = useState([] as CartItemType[]);
+
+
 	const { data, isLoading, error } = useQuery<CartItemType[]>(
 		"products",
 		getProducts
@@ -44,6 +48,7 @@ const App = () => {
 
 	return (
 		<Wrapper>
+			<Drawer anchor='right' open={cartOpen} onClose={() => setCartOpen(false) }
 			<Grid container spacing={3}>
 				{data?.map((item) => (
 					<Grid item key={item.id} xs={12} sm={4}>
